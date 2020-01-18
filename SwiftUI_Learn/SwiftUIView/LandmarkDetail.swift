5ec5e61ceb5ac8e0c13cc16d8a96e282dcc154a6
Ryuichi Fukami <fukaryu421@gmail.com>
2020-01-18 23:47:27 +0900

    add cell

---
+++ SwiftUI_Learn/SwiftUIView/LandmarkDetail.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUI_Learn
+//
+//  Created by 深見龍一 on 2020/01/18.
+//  Copyright © 2020 深見龍一. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
---
+++ SwiftUI_Learn/SwiftUIView/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  SwiftUI_Learn
+//
+//  Created by 深見龍一 on 2020/01/18.
+//  Copyright © 2020 深見龍一. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    var body: some View {
+        List(landmarkData) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
