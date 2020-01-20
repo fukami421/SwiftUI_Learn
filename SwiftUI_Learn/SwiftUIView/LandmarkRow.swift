8d5e4db6d1779d31b22dbcafa9a2c621644c2c53
Ryuichi Fukami <fukaryu421@gmail.com>
2020-01-20 15:34:24 +0900

    add binding to toggle control

--- SwiftUI_Learn/SwiftUIView/LandmarkRow.swift
+++ SwiftUI_Learn/SwiftUIView/LandmarkRow.swift
@@ -18,6 +18,12 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .imageScale(.medium)
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
@@ -25,8 +31,8 @@ struct LandmarkRow: View {
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            LandmarkRow(landmark: landmarkData[0])
             LandmarkRow(landmark: landmarkData[1])
+            LandmarkRow(landmark: landmarkData[2])
         }
         .previewLayout(.fixed(width: 300, height: 70))
     }
